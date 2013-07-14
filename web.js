273201b5e86d0a19087f72fdea2caf82c9461e0a
Wilson Sumanang <wilson.sumanang@zefr.com>
2013-07-14 16:17:32 -0700

    update web.js read from index

--- web.js
+++ web.js
@@ -2,8 +2,13 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs')
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(fs.readFileSync(index.html, function(err, data) {
+  	if(err) throw err;
+		console.log(data);
+  });
 });
 
 var port = process.env.PORT || 5000;
